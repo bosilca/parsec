bfbb78f2665110a707d365c394cff7c754174f4a
cflinto <cflint@unistra.fr>
2022-10-19 11:06:14 -0400

    add forgotten files

---
+++ parsec/task_class.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2012-2019 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+
+#include "parsec/task_class.h"
+
+
+void parsec_debug_dump_task_class_exec(parsec_task_class_t *tc)
+{
+
+}
---
+++ parsec/task_class.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012-2019 The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ */
+
+#ifndef PARSEC_TASK_CLASS_H_HAS_BEEN_INCLUDED
+#define PARSEC_TASK_CLASS_H_HAS_BEEN_INCLUDED
+
+#include "parsec/parsec_config.h"
+#include "parsec_internal.h"
+
+void parsec_debug_dump_task_class_exec(parsec_task_class_t *tc);
+
+#endif  /* PARSEC_TASK_CLASS_H_HAS_BEEN_INCLUDED */
