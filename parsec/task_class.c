781113d8d9604ad73101e9053dfb209d1866282c
cflinto <cflint@unistra.fr>
2022-10-24 14:40:56 -0400

    add various helper functions

--- parsec/task_class.c
+++ parsec/task_class.c
@@ -6,7 +6,6 @@
 
 #include "parsec/task_class.h"
 
-
 // Reminder: definitions:
 
 // /**
@@ -63,7 +62,6 @@
 //     parsec_hook_t               *fini;
 // };
 
-
 // struct parsec_flow_s {
 //     char               *name;
 //     uint8_t             sym_type;
@@ -95,16 +93,192 @@
 //                                                 *   this dep is a direct memory access */
 // };
 
-
-
-
-bool parsec_helper_flow_is_in_flow_array(const parsec_flow_t *flow, parsec_flow_t *flow_array[], int flow_array_size) {
-    for (int i = 0; i < flow_array_size; i++) {
-        if (flow == flow_array[i]) {
+bool parsec_helper_flow_is_in_flow_array(const parsec_flow_t *flow, parsec_flow_t *flow_array[], int flow_array_size)
+{
+    for (int i = 0; i < flow_array_size; i++)
+    {
+        if (flow == flow_array[i])
+        {
             return true;
         }
     }
     return false;
+}
+
+/** __parsec_LBM_shift_all_flow_reference_after
+ *
+ * Shift all the flows after "pivot_flow" by "shift"
+ *
+ */
+void parsec_shift_all_flows_after(parsec_task_class_t *tc, parsec_flow_t *pivot_flow, int shift)
+{
+    int i, j, k;
+    int flow_in_out, dep_in_out;
+    int pivot_index;
+    parsec_flow_t *flow;
+    parsec_dep_t *dep;
+
+    // use an array to keep track of the flows we already treated
+    parsec_flow_t *treated_flows[MAX_PARAM_COUNT];
+    int treated_flows_size = 0;
+
+    // Increase the IDs of every flow that is greater than the ID of pivot_flow
+    for (flow_in_out = 0; flow_in_out < 2; ++flow_in_out)
+    {
+        for (i = 0; i < MAX_PARAM_COUNT; i++)
+        {
+            flow = (parsec_flow_t *)(flow_in_out ? tc->out[i] : tc->in[i]);
+            if (parsec_helper_flow_is_in_flow_array(flow, treated_flows, treated_flows_size))
+            {
+                continue;
+            }
+
+            if (flow)
+            {
+                if (flow->flow_index > pivot_flow->flow_index)
+                {
+                    flow->flow_index += shift;
+                }
+
+                treated_flows[treated_flows_size] = flow;
+                ++treated_flows_size;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    /* We perform the shift on both the in and out flows of the task class
+     * The pivot flow can have different indices in the in and out arrays
+     *
+     * Note: We displace all the flows following the pivot flow in the in and out array
+     * But this is just for convience, to get the parametrized subflows next to each other
+     */
+    for (flow_in_out = 0; flow_in_out < 2; ++flow_in_out)
+    {
+
+        // Look for the pivot flow
+        pivot_index = -1;
+        for (i = 0; i < MAX_PARAM_COUNT; i++)
+        {
+            flow = (parsec_flow_t *)(flow_in_out ? tc->out[i] : tc->in[i]);
+
+            if (!flow)
+            {
+                break;
+            }
+
+            if (flow == pivot_flow)
+            {
+                pivot_index = i;
+                break;
+            }
+        }
+
+        // If the pivot was not found in this flow_in_out, skip the shift
+        if (pivot_index == -1)
+        {
+            continue;
+        }
+
+        // Find the last non-null flow
+        for (i = pivot_index; i < MAX_PARAM_COUNT; i++)
+        {
+            flow = (parsec_flow_t *)(flow_in_out ? tc->out[i] : tc->in[i]);
+
+            if (!flow)
+            {
+                break;
+            }
+        }
+        int last_flow_index = i - 1;
+
+        // Shift all the flows after the pivot
+        for (i = last_flow_index; i > pivot_index; i--)
+        {
+            flow = (parsec_flow_t *)(flow_in_out ? tc->out[i] : tc->in[i]);
+
+            assert(flow); // We should not have a NULL flow here
+            // assert(flow->flow_index == i); // The flow index can acutally be anything
+            assert(i + shift < MAX_PARAM_COUNT); // We should not overflow the array
+
+            // Shift the flow
+            (flow_in_out ? tc->out : tc->in)[i + shift] = flow;
+        }
+    }
+}
+
+void parsec_shift_all_deps_after(parsec_flow_t *flow, int dep_in_out, parsec_dep_t *pivot_dep, int shift)
+{
+    int pivot_dep_index;
+
+    // Look for the pivot dep
+    pivot_dep_index = -1;
+    for (int i = 0; i < dep_in_out?MAX_DEP_OUT_COUNT:MAX_DEP_IN_COUNT; i++)
+    {
+        parsec_dep_t *dep = (parsec_dep_t *)(dep_in_out ? flow->dep_out[i] : flow->dep_in[i]);
+
+        if (!dep)
+        {
+            break;
+        }
+
+        if (dep == pivot_dep)
+        {
+            pivot_dep_index = i;
+            break;
+        }
+    }
+
+    // If the pivot was not found in this flow_in_out, skip the shift
+    if (pivot_dep_index == -1)
+    {
+        return;
+    }
+
+    assert((dep_in_out ? flow->dep_out[pivot_dep_index] : flow->dep_in[pivot_dep_index]) == pivot_dep); // From there, the pivot dep should be in the array
+
+    // Find the last non-null dep
+    int last_dep_index;
+    for (last_dep_index = pivot_dep_index + 1; last_dep_index < dep_in_out?MAX_DEP_OUT_COUNT:MAX_DEP_IN_COUNT; ++last_dep_index)
+    {
+        parsec_dep_t *dep = (parsec_dep_t *)(dep_in_out ? flow->dep_out[last_dep_index] : flow->dep_in[last_dep_index]);
+
+        if (!dep)
+        {
+            break;
+        }
+    }
+    last_dep_index--;
+
+    // Shift all the deps after the pivot
+    for (int i = last_dep_index; i > pivot_dep_index; i--)
+    {
+        parsec_dep_t *dep = (parsec_dep_t *)(dep_in_out ? flow->dep_out[i] : flow->dep_in[i]);
+
+        assert(dep); // We should not have a NULL dep here
+        // assert(dep->dep_index == i); // The dep index can acutally be anything
+        assert(i + shift < dep_in_out?MAX_DEP_OUT_COUNT:MAX_DEP_IN_COUNT); // We should not overflow the array
+
+        dep->dep_index += shift;
+
+        // Shift the dep
+        (dep_in_out ? flow->dep_out : flow->dep_in)[i + shift] = dep;
+    }
+
+    // Also shift the flow's flow_datatype_mask
+    if(dep_in_out)
+    {
+        // If output dep
+        // Goal: 000xxxxx -> 0xxxx11x (if shift=1 and pivot_dep_index=1)
+        // (add 1's in between)
+        parsec_dependency_t unshifted_values = flow->flow_datatype_mask & ((1<<pivot_dep_index)-1);
+        parsec_dependency_t shifted_values = (flow->flow_datatype_mask >> (pivot_dep_index + 1)) << (pivot_dep_index + 1 + shift);
+        parsec_dependency_t in_between = ((1<<(shift+1))-1) << pivot_dep_index;
+        flow->flow_datatype_mask = unshifted_values | shifted_values | in_between;
+    }
 }
 
 /* Prints the task class information (for debugging purposes)
@@ -124,36 +298,38 @@ void parsec_debug_dump_task_class_at_exec(parsec_task_class_t *tc)
     parsec_debug_verbose(1, parsec_debug_output, "###### PRINTING TASK CLASS %s ######", tc->name);
 
     parsec_debug_verbose(1, parsec_debug_output, "Task Class %s (%p) has %d flows, %d parameters, %d locals",
-                         tc->name, (void*)tc, tc->nb_flows, tc->nb_parameters, tc->nb_locals);
+                         tc->name, (void *)tc, tc->nb_flows, tc->nb_parameters, tc->nb_locals);
 
-    for(i = 0; i < MAX_PARAM_COUNT; i++) {
-        for(flow_in_out=0;flow_in_out<2;++flow_in_out)
+    for (i = 0; i < MAX_PARAM_COUNT; i++)
+    {
+        for (flow_in_out = 0; flow_in_out < 2; ++flow_in_out)
         {
-            flow = (parsec_flow_t*)(flow_in_out?tc->out[i]:tc->in[i]);
+            flow = (parsec_flow_t *)(flow_in_out ? tc->out[i] : tc->in[i]);
 
-            if(flow && !parsec_helper_flow_is_in_flow_array(flow, treated_flows, treated_flows_size))
+            if (flow && !parsec_helper_flow_is_in_flow_array(flow, treated_flows, treated_flows_size))
             {
                 parsec_debug_verbose(1, parsec_debug_output, "  flow %s (addr=%p, id=%d, flow_datatype_mask=%p, flow_flags=%p)",
-                                        flow->name, (void*)flow, flow->flow_index, flow->flow_datatype_mask, flow->flow_flags);
-                for(dep_in_out=0;dep_in_out<2;++dep_in_out)
+                                     flow->name, (void *)flow, flow->flow_index, flow->flow_datatype_mask, flow->flow_flags);
+                for (dep_in_out = 0; dep_in_out < 2; ++dep_in_out)
                 {
-                    for(j = 0; j < (dep_in_out?MAX_DEP_OUT_COUNT:MAX_DEP_IN_COUNT); j++) {
-                        dep = (parsec_dep_t*)(dep_in_out?flow->dep_out[j]:flow->dep_in[j]);
-                        if(!dep)
+                    for (j = 0; j < (dep_in_out ? MAX_DEP_OUT_COUNT : MAX_DEP_IN_COUNT); j++)
+                    {
+                        dep = (parsec_dep_t *)(dep_in_out ? flow->dep_out[j] : flow->dep_in[j]);
+                        if (!dep)
                         {
                             continue;
                         }
 
-                        if( PARSEC_LOCAL_DATA_TASK_CLASS_ID == dep->task_class_id ) {
+                        if (PARSEC_LOCAL_DATA_TASK_CLASS_ID == dep->task_class_id)
+                        {
                             parsec_debug_verbose(1, parsec_debug_output, "    %s dep [%d] of flow %s linked with data collection",
-                                                dep_in_out?"output":"input", j, flow->name);
-
+                                                 dep_in_out ? "output" : "input", j, flow->name);
                         }
-                        else if(dep->flow)
+                        else if (dep->flow)
                         {
                             parsec_debug_verbose(1, parsec_debug_output, "    %s dep [%d] of flow %s is a dep that is linked to dep %d of flow %s (id=%d) of task class %d",
-                                                dep_in_out?"output":"input", j, flow->name, dep->dep_index, dep->flow->name,
-                                                dep->flow->flow_index, dep->task_class_id);
+                                                 dep_in_out ? "output" : "input", j, flow->name, dep->dep_index, dep->flow->name,
+                                                 dep->flow->flow_index, dep->task_class_id);
                         }
                         else
                         {
@@ -161,7 +337,7 @@ void parsec_debug_dump_task_class_at_exec(parsec_task_class_t *tc)
                             continue;
                         }
                         parsec_debug_verbose(1, parsec_debug_output, "      datatype=%d, direct_data=%p, dep_datatype_index=%d, dep_index=%d, task_class_id=%d",
-                                            dep->dep_datatype_index, (void*)dep->direct_data, dep->dep_datatype_index, dep->dep_index, dep->task_class_id);
+                                             dep->dep_datatype_index, (void *)dep->direct_data, dep->dep_datatype_index, dep->dep_index, dep->task_class_id);
                     }
                 }
 
@@ -183,14 +359,15 @@ void parsec_check_sanity_of_task_class(parsec_task_class_t *tc)
 
     // flows can appear twice in a task class (if both in and out)
     parsec_flow_t *treated_flows[MAX_PARAM_COUNT];
-    int treated_flows_size = 0;;
+    int treated_flows_size = 0;
 
-    for(i = 0; i < MAX_PARAM_COUNT; i++) {
-        for(flow_in_out=0;flow_in_out<2;++flow_in_out)
+    for (i = 0; i < MAX_PARAM_COUNT; i++)
+    {
+        for (flow_in_out = 0; flow_in_out < 2; ++flow_in_out)
         {
-            flow = (parsec_flow_t*)(flow_in_out?tc->out[i]:tc->in[i]);
+            flow = (parsec_flow_t *)(flow_in_out ? tc->out[i] : tc->in[i]);
 
-            if(flow && !parsec_helper_flow_is_in_flow_array(flow, treated_flows, treated_flows_size))
+            if (flow && !parsec_helper_flow_is_in_flow_array(flow, treated_flows, treated_flows_size))
             {
                 // TODO ? Is there anything to assert?
 
@@ -201,55 +378,67 @@ void parsec_check_sanity_of_task_class(parsec_task_class_t *tc)
     }
 
     // Check the coherency of the flow flags
-    for(i = 0; i < MAX_PARAM_COUNT; i++) {
+    for (i = 0; i < MAX_PARAM_COUNT; i++)
+    {
         flow = tc->out[i];
 
-        if(!flow)
+        if (!flow)
         {
             break;
         }
 
         // For each output dep of the flow ...
-        for(j = 0; j < (dep_in_out?MAX_DEP_OUT_COUNT:MAX_DEP_IN_COUNT); j++) {
+        for (j = 0; j < (dep_in_out ? MAX_DEP_OUT_COUNT : MAX_DEP_IN_COUNT); j++)
+        {
             dep = flow->dep_out[j];
-            if(!dep)
+            if (!dep)
             {
                 break;
             }
 
             // All out dependencies should be in the flow datatype mask
-            assert((1<<dep->dep_index) & flow->flow_datatype_mask);
+            assert((1 << dep->dep_index) & flow->flow_datatype_mask);
         }
     }
 
     assert(tc->nb_flows == treated_flows_size);
 }
 
-parsec_flow_t *parsec_helper_copy_flow(parsec_flow_t * flow)
+parsec_flow_t *parsec_helper_copy_flow(parsec_flow_t *flow)
 {
     int flow_in_out;
 
-    parsec_flow_t *new_flow = (parsec_flow_t*)malloc(sizeof(parsec_flow_t));
+    parsec_flow_t *new_flow = (parsec_flow_t *)malloc(sizeof(parsec_flow_t));
     assert(new_flow);
     memcpy(new_flow, flow, sizeof(parsec_flow_t));
 
     // Copy the dependencies
     int i;
-    for(flow_in_out=0;flow_in_out<2;++flow_in_out) {
-        for(i = 0; i < MAX_DEP_IN_COUNT; i++) {
-            parsec_dep_t *dep = (parsec_dep_t*)(flow_in_out?new_flow->dep_out[i]:new_flow->dep_in[i]);
+    for (flow_in_out = 0; flow_in_out < 2; ++flow_in_out)
+    {
+        for (i = 0; i < flow_in_out ? MAX_DEP_OUT_COUNT : MAX_DEP_IN_COUNT; i++)
+        {
+            parsec_dep_t *dep = (parsec_dep_t *)(flow_in_out ? new_flow->dep_out[i] : new_flow->dep_in[i]);
 
-            if(!dep)
+            if (!dep)
             {
                 break;
             }
 
-            parsec_dep_t *new_dep = (parsec_dep_t*)malloc(sizeof(parsec_dep_t));
+            parsec_dep_t *new_dep = (parsec_dep_t *)malloc(sizeof(parsec_dep_t));
             assert(new_dep);
             memcpy(new_dep, dep, sizeof(parsec_dep_t));
-            (flow_in_out?new_flow->dep_out:new_flow->dep_in)[i] = new_dep;
+            (flow_in_out ? new_flow->dep_out : new_flow->dep_in)[i] = new_dep;
         }
     }
 
     return new_flow;
+}
+
+parsec_dep_t *parsec_helper_copy_dep(parsec_dep_t * dep)
+{
+    parsec_dep_t *new_dep = (parsec_dep_t *)malloc(sizeof(parsec_dep_t));
+    assert(new_dep);
+    memcpy(new_dep, dep, sizeof(parsec_dep_t));
+    return new_dep;
 }
--- parsec/task_class.h
+++ parsec/task_class.h
@@ -11,6 +11,8 @@
 #include "parsec/interfaces/interface.h"
 #include "parsec_internal.h"
 
+void parsec_shift_all_flows_after(parsec_task_class_t *tc, parsec_flow_t *pivot_flow, int shift);
+void parsec_shift_all_deps_after(parsec_flow_t *flow, int dep_in_out, parsec_dep_t *pivot_dep, int shift);
 // flow_array should be const, but it creates a warning in the generated code
 bool parsec_helper_flow_is_in_flow_array(const parsec_flow_t *flow, parsec_flow_t *flow_array[], int flow_array_size);
 
@@ -19,5 +21,6 @@ void parsec_check_sanity_of_task_class(parsec_task_class_t *tc);
 
 // Copy a flow (including its deps) and returns a pointer to the new flow
 parsec_flow_t *parsec_helper_copy_flow(parsec_flow_t * flow);
+parsec_dep_t *parsec_helper_copy_dep(parsec_dep_t * dep);
 
 #endif  /* PARSEC_TASK_CLASS_H_HAS_BEEN_INCLUDED */
